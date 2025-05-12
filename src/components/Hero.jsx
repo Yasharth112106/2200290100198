5ed29aa873d7db217675f9c8c6f0d338efc1864e
Yasharth112106 <yasharth112106@gmail.com>
2025-05-13 00:11:19 +0530

    first commit

--- src/components/Hero.jsx
+++
@@ -1,6 +0,0 @@
-export default function Hero(){
-    return <div className="hero">
-        <h1>Online Experiences</h1>
-        <p>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
-    </div>
-}
